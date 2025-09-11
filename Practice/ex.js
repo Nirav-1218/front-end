790aca0d85da187509d5298200121eb0e19eefd5
Nirav-1218 <gujariyanirav@gmail.com>
2025-09-11 11:32:52 +0530

    newrepo

---
+++ Practice/ex.js
@@ -0,0 +1,12 @@
+
+// bom :- borwaer object model
+
+// window.alert("pls filed data...")
+// console.log(alert("hello"))
+// alert("Hello")
+
+// confirm("Are your sure ? ");
+
+// prompt(" 5 + 5 => ?")
+
+// print()
