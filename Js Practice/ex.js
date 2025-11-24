fb1a53a3cb53b0b87603bba8e0bc193372488cae
Nirav-1218 <gujariyanirav@gmail.com>
2025-11-24 13:04:36 +0530

    newrepo

---
+++ Js Practice/ex.js
@@ -0,0 +1,14 @@
+
+// console.log("HEllo external js");
+
+// bom :- browser object model 
+// pop box
+
+// alert("pls required form..!")
+// confirm("Are you sure ?")
+// prompt("5+ 5 => ?")
+// print()
+
+
+// innerHeight()
+// innerWidth()
